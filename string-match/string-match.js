98e4ac63ff5fca0c75c1dcc15fda4f991b06e97a
dewald_laubscher <laubscher.dewald@gmail.com>
2014-12-11 12:56:28 +0200

    hopefully break strings into an array with sub array containing single charas

--- string-match/string-match.js
+++ string-match/string-match.js
@@ -1,11 +1,21 @@
 module.exports = {
+	/*this function returns percentage value*/
 	stringMatch: function (compareString, toString) {
-		
+		var percentage = 0; //0-1
+		return percentage;
 	}
 		//wondering if this would be feasable as a helper in a module
-		function breakString() {
-			
-			return [];
+		function breakString(string) {
+			var array = string.split(' ');
+			var innerArray = [];
+
+			for(i in array) {
+				for (ii in array[i]) {
+					innerArray[ii] = array[i][ii];
+				}
+				array[i] = innerArray[ii];
+			}
+			return array;
 		}
 
 
