50ed5b8a7da3834835527e18838f3240edac26ae
dewald_laubscher <laubscher.dewald@gmail.com>
2014-12-11 12:37:33 +0200

    add break string helper function

--- string-match/string-match.js
+++ string-match/string-match.js
@@ -2,4 +2,11 @@ module.exports = {
 	stringMatch: function (compareString, toString) {
 		
 	}
+		//wondering if this would be feasable as a helper in a module
+		function breakString() {
+			
+			return [];
+		}
+
+
 };
