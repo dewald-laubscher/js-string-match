81e9d408ac50b0ada8bdece4446de2b6f412a758
dewald_laubscher <laubscher.dewald@gmail.com>
2014-12-11 13:00:30 +0200

    break the two strings to be compared

--- string-match/string-match.js
+++ string-match/string-match.js
@@ -2,6 +2,9 @@ module.exports = {
 	/*this function returns percentage value*/
 	stringMatch: function (compareString, toString) {
 		var percentage = 0; //0-1
+		compareArray = breakString(compareString);
+		toArray = breakString(toString);
+
 		return percentage;
 	}
 		//wondering if this would be feasable as a helper in a module
