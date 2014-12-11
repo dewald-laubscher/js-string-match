76c75c722e996228ff8943c398a37a85b7d589d0
dewald_laubscher <laubscher.dewald@gmail.com>
2014-12-11 14:05:58 +0200

    fix the structure for string matching function

--- string-match/string-match.js
+++ string-match/string-match.js
@@ -1,3 +1,17 @@
+//wondering if this would be feasable as a helper in a module
+var breakString = function (string) {
+
+	var array = string.toLowerCase().split(' ');
+	var innerArray = [];
+
+	for(i in array) {
+		for (ii in array[i]) {
+			innerArray[ii] = array[i][ii];
+		}
+		array[i] = innerArray[ii];
+	}
+	return array;
+}
 module.exports = {
 	/*this function returns percentage value*/
 	stringMatch: function (compareString, toString, options) {
@@ -5,23 +19,9 @@ module.exports = {
 
 		var percentage = 0; //0-1
 
-		compareArray = _breakString: compareString;
-		toArray = _breakString: toString;
+		compareArray = breakString(compareString);
+		toArray = breakString(toString);
 
 		return percentage;
 	}
-		//wondering if this would be feasable as a helper in a module
-		var breakString = function (string) {
-
-			var array = toLowerCase(string).split(' ');
-			var innerArray = [];
-
-			for(i in array) {
-				for (ii in array[i]) {
-					innerArray[ii] = array[i][ii];
-				}
-				array[i] = innerArray[ii];
-			}
-			return array;
-		}
 };
